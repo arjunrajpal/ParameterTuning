e1068c74b99579b5574d1383390274ba7bc30a03
dushyantRathore <dushyant.bgs@gmail.com>
2017-04-19 15:44:55 +0530

    Updated

--- code/DifferentialEvolution_all.py
+++ code/DifferentialEvolution_all.py
@@ -5,60 +5,37 @@ import pandas as pd
 import numpy as nump
 import math
 from data import data
+import random
 
 # np, f, cr, life, Goal, noOfParameters = 10, 0.75, 0.3, 5, [], 0 #input
 # sBest = [] #ouput
 
+global_best_score = []
+global_best = []
+
 
 # Runs Random Forest on the dataset using parameters present in candidate
-def random_forest(a,b,c,d,candidate):
-    # print candidate
+def random_forest(a, b, c, d, candidate):
+    print "Candidate in Random Forest : " + str(candidate)
+
+    print "Max feature : " + str(candidate["tunings"][0])
+    print "Max leaf nodes : " + str(candidate["tunings"][1])
+    print "Min sample split : " + str(candidate["tunings"][2])
+    print "Min samples leaf : " + str(candidate["tunings"][3])
+    print "Max no of estimators : " + str(candidate["tunings"][4])
+
     rf = RandomForestClassifier(max_features=candidate['tunings'][0], max_leaf_nodes=candidate['tunings'][1], min_samples_split=candidate['tunings'][2], min_samples_leaf=candidate['tunings'][3], n_estimators=candidate['tunings'][4])
-    rf.fit(a,b)
+    rf.fit(a, b)
     pred = rf.predict(c)
     # print "Predicted Matrix : "  + str(pred)
-    p = precision_score(d, pred, average='macro')
+    p = precision_score(d, pred, average='weighted')
 
     return p
 
 
-# Runs CART on the dataset
-# def cart(a,b,c,d):
-#     ct = DecisionTreeClassifier()
-#     ct.classes_ = nump.arange(1,29)
-#     ct.fit(a,b)
-#     pred = ct.predict(c)
-#     # print "Predicted Matrix : "  + str(pred)
-#     p = precision_score(d, pred, average='macro')
-#
-#     return p
-
-# Determines which algo to run depending on the value of mlago
-# def runAlgo(X_Train, Y_Train, X_Test, Y_Test, candidate, mlalgo):
-#
-#     if mlalgo == 0:
-#         p = random_forest(X_Train, Y_Train, X_Test, Y_Test, candidate)
-#     elif mlalgo == 1:
-#         p = cart(X_Train, Y_Train, X_Test, Y_Test, candidate)
-#
-#     return p
-
-
-# Limits the newly computed value to the legal range min...max of that parameter (decision)
-def trim(decision, computed):
-    return max(decision['high'], min(computed, decision['low']))
-
-
-# Retrieves the appropriate dataset from readDataset function present in data.py and passes the Training and Testing dataset to Random Forest function
+# Retrieves the appropriate dataset from readDataset function present in data.py and passes
+# the Training and Testing dataset to Random Forest function
 def score(candidate, datasets):
-
-    # # antV1
-    #
-    # f1 = pd.read_csv("../final_dataset/ant/2.csv", delimiter=",")
-    # df1 = pd.DataFrame(f1)
-    #
-    # f2 = pd.read_csv("../final_dataset/ant/4.csv", delimiter=",")
-    # df2 = pd.DataFrame(f2)
 
     df1, df2 = data.readDataset(datasets)
 
@@ -76,25 +53,63 @@ def score(candidate, datasets):
     # print X_Test
     # print Y_Test
 
-    # p = random_forest(X_Train, Y_Train, X_Test, Y_Test, candidate)
+    p = random_forest(X_Train, Y_Train, X_Test, Y_Test, candidate)
+
+    return p
+
+
+def score_test(candidate, datasets):
+
+    df1, df2 = data.readDataset_for_testing(datasets)
+
+    # Training Set
+    X_Train_DF = df1.ix[:, 3:23]
+    X_Train = X_Train_DF.values.astype(float)
+    Y_Train = nump.asarray(list(df1["bug"]))
+    # print X_Train
+    # print Y_Train
+
+    # Testing Set
+    X_Test_DF = df2.ix[:, 3:23]
+    X_Test = X_Test_DF.values.astype(float)
+    Y_Test = nump.asarray(list(df2["bug"]))
+    # print X_Test
+    # print Y_Test
 
     p = random_forest(X_Train, Y_Train, X_Test, Y_Test, candidate)
 
     return p
 
-# Initialises the population such that each parameter of each candidate in the population gets a random value from the parameter's valid range
+
+# Initialises the population such that each parameter of each candidate
+# in the population gets a random value from the parameter's valid range
 def initialisePopulation(np,noOfParameters):
     population = []
 
     for i in range(0,np):
         candidate = {}
         tunings = []
-        # tunings.append(nump.random.uniform(0.01,1))
-        tunings.append(nump.random.uniform(algoParameters[1]['low'],algoParameters[1]['high']))
-        tunings.append(nump.random.randint(algoParameters[2]['low'],algoParameters[2]['high']))
-        tunings.append(nump.random.randint(algoParameters[3]['low'],algoParameters[3]['high']))
-        tunings.append(nump.random.randint(algoParameters[4]['low'],algoParameters[4]['high']))
-        tunings.append(nump.random.randint(algoParameters[5]['low'],algoParameters[5]['high']))
+        # tunings.append(nump.random.uniform(algoParameters[0]['low'], algoParameters[0]['high']))
+
+        max_feature = int(nump.random.uniform(algoParameters[0]['low'], algoParameters[0]['high']))
+        print "Max Feature selected for index " + str(i) + " : " + str(max_feature)
+        tunings.append(max_feature)
+
+        max_leaf_nodes = int(nump.random.uniform(algoParameters[1]['low'], algoParameters[1]['high']))
+        print "Max Leaf Nodes for index " + str(i) + " : " + str(max_leaf_nodes)
+        tunings.append(max_leaf_nodes)
+
+        min_sample_split = int(nump.random.uniform(algoParameters[2]['low'], algoParameters[2]['high']))
+        print "Min sample split for index " + str(i) + " : " + str(min_sample_split)
+        tunings.append(min_sample_split)
+
+        min_samples_leaf = int(nump.random.uniform(algoParameters[3]['low'], algoParameters[3]['high']))
+        print "Min samples leaf for index " + str(i) + " : " + str(min_samples_leaf)
+        tunings.append(min_samples_leaf)
+
+        n_estimators = int(nump.random.uniform(algoParameters[4]['low'], algoParameters[4]['high']))
+        print "n_estimators for index " + str(i) + " : " + str(n_estimators) + "\n"
+        tunings.append(n_estimators)
 
         # population[i]['tunings']
         # print tunings[4]
@@ -103,23 +118,24 @@ def initialisePopulation(np,noOfParameters):
 
         population.append(candidate)
 
-    # print population
+    print "Population"
+    print population
 
     return population
 
 
-# Returns three random candidates from a population of size 10 such that neither of them is equal to the target candidate i.e old
-def threeOthers(pop,old):
+# Returns three random candidates from a population of size 10 such that
+# neither of them is equal to the target candidate i.e old
+def threeOthers(pop,old,index):
 
-    while(1):
-        three = nump.random.randint(0, 10, size=(3))
-        three = list(three)
+    # while(1):
+    three = range(0,10,1)
+    three.remove(index)
+    three = random.sample(three, 3)  # Array Formation
 
-        # print three
-
-        if three[0] != three[1] and three[1] != three[2] and three[0] != three[2]:
-            if (pop[three[0]] != old) and (pop[three[1]] != old) and (pop[three[2]] != old):
-                break
+    # print pop[three[0]['tunings']]
+    # print pop[three[0]['tunings']]
+    # print pop[three[0]['tunings']]
 
     return pop[three[0]]['tunings'], pop[three[1]]['tunings'], pop[three[2]]['tunings']
 
@@ -132,13 +148,16 @@ def improve(population, oldGeneration):
         return False
 
 
-# Compares the precision score of each candidate in the population and returns the candidate with the highest precision score as the Best Solution
+# Compares the precision score of each candidate in the population and
+# returns the candidate with the highest precision score as the Best Solution
 def getBestSolution(population):
     max = -1
     bestSolution = {}
 
     for i in range(0,len(population)):
         scores = population[i]['score']
+        # print scores
+
         if scores > max:
             max = scores
             bestSolution = population[i]
@@ -146,93 +165,102 @@ def getBestSolution(population):
     return bestSolution
 
 
-# Creates a mutant whose each parameter value is either taken from the target candidate(old) or is computed using (a+f*(b-c)) depending on value of cr
-def extrapolate(old, pop, cr, f, noOfParameters):
-    a, b, c = threeOthers(pop,old)
+# Creates a mutant whose each parameter value is either taken from the target candidate(old) or is
+# computed using (a+f*(b-c)) depending on value of cr
+def extrapolate(old, pop, cr, f, noOfParameters, index):
+    a, b, c = threeOthers(pop, old, index)  # index is for the target row
+
     newf = []
 
-    # print a,b,c
-    for i in range(0,noOfParameters):
-        if cr < nump.random.uniform(0,1):
+    print "\nThe other 3 selected rows for index " + str(index) + " : "
+    print a
+    print b
+    print c
+
+    for i in range(0, noOfParameters):
+
+        x = nump.random.uniform(0, 1)
+        print "Random number for comparison with cr : " + str(x)
+
+        if cr < x:
+            print "Old tuning Value for index " + str(index) + " : " + (str(old['tunings'][i]))
             newf.append(old['tunings'][i])
+
         elif type(old['tunings'][i]) == bool:
                 newf.append(not old['tunings'][i])
         else:
-            newf.append(trim(algoParameters[i+1],a[i]+f*(b[i]-c[i])))
+            lo = algoParameters[i]["low"]
+            hi = algoParameters[i]["high"]
+            value = a[i] + (f * (b[i] - c[i]))
 
-    # newf[1] = int(newf[1])
-    # newf[2] = int(newf[2])
-    # newf[3] = int(newf[3])
-    # newf[4] = int(newf[4])
+            print "Value before trim : " + str(value)
+            mutant_value = int(max(lo, min(value, hi)))
 
-    # if(newf[0] < 0.01) or (newf[0] > 1):
-    #     newf[0] = math.m(0.01,1)
-    #
-    # if(newf[1] < 1) or (newf[1] > 50):
-    #     newf[1] = nump.random.randint(1,50)
-    #
-    # if (newf[2] < 2) or (newf[2] > 20):
-    #     newf[2] = nump.random.randint(2,20)
-    #
-    # if (newf[3] < 1) or (newf[3] > 20):
-    #     newf[3] = nump.random.randint(1,20)
-    #
-    # if (newf[4] < 50) or (newf[4] > 150):
-    #     newf[4] = nump.random.randint(50,150)
+            print "Mutant Value : " + str(mutant_value)
 
-    newCandidate = {'score':0, 'tunings':newf}
+            newf.append(mutant_value)
 
+    score_original = score(old, datasets)
+
+    dict_mutant = {'tunings': newf}
+    score_mutant = score(dict_mutant, datasets)
+
+    print "Original Score : " + str(score_original)
+    print "Mutant Score : " + str(score_mutant)
+
+    if score_mutant > score_original:
+        global_best_score.append(score_mutant)
+        global_best.append({'score': score_mutant, 'tunings': newf})
+
+    else:
+        global_best_score.append(score_original)
+        global_best.append({'score': score_original, 'tunings': old["tunings"]})
+
+    print global_best
+
+    # newCandidate = {'score': 0, 'tunings': newf}
     # print newCandidate
-
-    return newCandidate
+    # return newCandidate
 
 
 # Performs Differential Evolution
 def DE(np, f, cr, life, noOfParameters, datasets):
 
-    sBest = []
-    population = initialisePopulation(np,noOfParameters)
-    sBest = getBestSolution(population)
+    population = initialisePopulation(np, noOfParameters)  # Intial population formation
 
-    while life > 0:
-        newGeneration = []
+    # while life > 0:
+    #
+    #     global_best = []
+    #     global_best_score = []
 
-        for i in range(0,np):
-            s = extrapolate(population[i],population,cr,f,noOfParameters)
+    for i in range(0, np):
+        extrapolate(population[i], population, cr, f, noOfParameters, i)
 
-            score_s = score(s,datasets)
-            score_population_i = score(population[i],datasets)
+    print global_best
 
-            s['score'] = score_s
-            population[i]['score'] = score_population_i
+    # if population["tunings"] != global_best["tunings"]:
+    #     population = global_best
+    # else:
+    #   life -= 1
 
-            if score_s > score_population_i:
-                newGeneration.append(s)
-            else:
-                newGeneration.append(population[i])
+    s_Best = getBestSolution(global_best)
 
-        oldGeneration = population
-        population = newGeneration
-
-        if not improve(population, oldGeneration):
-            life -= 1
-
-        sBest = getBestSolution(population)
-        # print sBest
-    return sBest
-
-
-# initialisePopulation(10,6)
-# print "Best Parameters for Random Forest", DE(10, 0.75, 0.3, 5, 5)
+    return s_Best
 
 # Sets the valid range for each parameter of the machine learning algorithm
-algoParameters = [{'low':0.01, 'high':1}, {'low':0.01, 'high':1}, {'low':1, 'high':50}, {'low':2, 'high':20}, {'low':1, 'high':20}, {'low':50, 'high':150}]
+algoParameters = [{'low': 1, 'high': 17}, {'low': 2, 'high': 50}, {'low': 2, 'high': 20}, {'low': 1, 'high': 20}, {'low': 50, 'high': 150}]
 
 # Invokes DE
-for datasets in range(17):
-    parameters = DE(10, 0.75, 0.3, 5, 5, datasets)
 
-    print "Best Parameters for Random Forest are ", parameters
+datasets = raw_input("Enter dataset : ")
+datasets = int(datasets)
+
+
+parameters = DE(10, 0.75, 0.3, 5, 5, datasets)
+
+print "\nBest Parameters for Random Forest in dataset ",datasets," are ", parameters
+print score_test(parameters, datasets)
+
 
 
 
--- code/data/data.py
+++ code/data/data.py
@@ -1,32 +1,191 @@
 import pandas as pd
 
+
 def readDataset(datasets):
 
     if datasets == 0:
         # antV0
 
-        f1 = pd.read_csv("../final_dataset/ant/1.csv", delimiter=",")
+        f1 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/1.csv", delimiter=",")
         df1 = pd.DataFrame(f1)
 
-        f2 = pd.read_csv("../final_dataset/ant/2.csv", delimiter=",")
+        f2 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/2.csv", delimiter=",")
         df2 = pd.DataFrame(f2)
 
     elif datasets == 1:
         # antV1
 
-        f1 = pd.read_csv("../final_dataset/ant/2.csv", delimiter=",")
+        f1 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/2.csv", delimiter=",")
         df1 = pd.DataFrame(f1)
 
-        f2 = pd.read_csv("../final_dataset/ant/3.csv", delimiter=",")
+        f2 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/3.csv", delimiter=",")
         df2 = pd.DataFrame(f2)
 
     elif datasets == 2:
         # antV2
 
-        f1 = pd.read_csv("../final_dataset/ant/3.csv", delimiter=",")
+        f1 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/3.csv", delimiter=",")
         df1 = pd.DataFrame(f1)
 
-        f2 = pd.read_csv("../final_dataset/ant/4.csv", delimiter=",")
+        f2 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/4.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 3:
+        # camelV0
+
+        f1 = pd.read_csv("../final_dataset/camel/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/camel/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 4:
+        # camelV1
+
+        f1 = pd.read_csv("../final_dataset/camel/2.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/camel/3.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 5:
+        # ivy
+
+        f1 = pd.read_csv("../final_dataset/ivy/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/ivy/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 6:
+        # jeditV0
+
+        f1 = pd.read_csv("../final_dataset/jedit/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/jedit/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 7:
+        # jeditV1
+
+        f1 = pd.read_csv("../final_dataset/jedit/2.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/jedit/3.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 8:
+        # jeditV2
+
+        f1 = pd.read_csv("../final_dataset/jedit/3.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/jedit/4.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 9:
+        # log4j
+
+        f1 = pd.read_csv("../final_dataset/log4j/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/log4j/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 10:
+        # lucene
+
+        f1 = pd.read_csv("../final_dataset/lucene/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/lucene/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 11:
+        # poiV0
+
+        f1 = pd.read_csv("../final_dataset/poi/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/poi/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 12:
+        # poiV1
+
+        f1 = pd.read_csv("../final_dataset/poi/2.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/poi/3.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 13:
+        # synapse
+
+        f1 = pd.read_csv("../final_dataset/synapse/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/synapse/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 14:
+        # velocity
+
+        f1 = pd.read_csv("../final_dataset/velocity/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/velocity/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 15:
+        # xercesV0
+
+        f1 = pd.read_csv("../final_dataset/xerces/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/xerces/2.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 16:
+        # xercesV1
+
+        f1 = pd.read_csv("../final_dataset/xerces/2.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("../final_dataset/xerces/3.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    return df1,df2
+
+
+def readDataset_for_testing(datasets):
+
+    if datasets == 0:
+        # antV0
+
+        f1 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/1.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/3.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 1:
+        # antV1
+
+        f1 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/2.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/4.csv", delimiter=",")
+        df2 = pd.DataFrame(f2)
+
+    elif datasets == 2:
+        # antV2
+
+        f1 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/3.csv", delimiter=",")
+        df1 = pd.DataFrame(f1)
+
+        f2 = pd.read_csv("/home/dushyant/Desktop/Github/ParameterTuning/uncombined_dataset_modified/ant/4.csv", delimiter=",")
         df2 = pd.DataFrame(f2)
 
     elif datasets == 3:
